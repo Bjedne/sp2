10e8ce296fd7254201c68c59650780086d84a513
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-05-06 14:25:14 +0200

    Content now correctly displays fetched items from the API

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ async function landingListings() {
     console.log(listings);
 
     listings.data.slice(0, 4).forEach(function (listing) {
-      landingContainer.innerHTML += `<div><h1>Here are some words to test ${listing.title}</h1></div>`;
+      landingContainer.innerHTML += `<div class="col-5">
+      <img src="${listing.media[0].url}">
+      <h3>${listing.title}</h3>
+      <p>100 credits</p>
+      <p>Ends: ${listing.endsAt}</p>
+        </div>`;
     });
   } catch (error) {
     console.log(error);
