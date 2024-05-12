f716e8528813de6ae7fbf302dfc5dc11c0b146c7
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-05-12 20:18:20 +0200

    Changed order in meta data so that the custom scss would apply and further configured the thumbnail images

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ async function landingListings() {
     const listings = await getListings();
     console.log(listings);
 
-    listings.data.slice(6, 10).forEach(function (listing) {
+    const filteredListings = listings.data.filter(
+      (listing) => listing.media.length > 0,
+    );
+
+    filteredListings.slice(0, 17).forEach(function (listing) {
       const endDate = new Date(listing.endsAt);
-      const formattedEndDate = endDate.toLocaleDateString();
+      const options = { day: "numeric", month: "numeric", year: "2-digit" };
+      const formattedEndDate = endDate.toLocaleDateString(undefined, options);
 
       landingContainer.innerHTML += `<div class="col-5">
       <img class="thumbnailImage" src="${listing.media[0].url}">
       <h3>${listing.title}</h3>
-      <p>100 credits</p>
-      <p>Ends: ${formattedEndDate}</p>
+      <div class="d-flex">
+        <p class="me-3">100 credits</p>
+        <p>Ends: ${formattedEndDate}</p>
+      </div>
         </div>`;
     });
   } catch (error) {
