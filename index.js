f2b56eaebc4d580212a4c2a3bddde6bb4726ace8
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-05-12 18:02:53 +0200

    Correctly set up scss file, changed size of images and formatted the auction deadline to be more readable with js

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ async function landingListings() {
     const listings = await getListings();
     console.log(listings);
 
-    listings.data.slice(0, 4).forEach(function (listing) {
+    listings.data.slice(6, 10).forEach(function (listing) {
+      const endDate = new Date(listing.endsAt);
+      const formattedEndDate = endDate.toLocaleDateString();
+
       landingContainer.innerHTML += `<div class="col-5">
-      <img src="${listing.media[0].url}">
+      <img class="thumbnailImage" src="${listing.media[0].url}">
       <h3>${listing.title}</h3>
       <p>100 credits</p>
-      <p>Ends: ${listing.endsAt}</p>
+      <p>Ends: ${formattedEndDate}</p>
         </div>`;
     });
   } catch (error) {
